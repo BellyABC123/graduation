5b3370dae6b9f3276025369cf7aa484a4a8362f1
wanzhang <1194451658@qq.com>
2013-05-13 05:35:51 +0800

    Add A Bridge In CutTheRopeScene

--- graduation/proj.win32/CutTheRopeScene.cpp
+++ graduation/proj.win32/CutTheRopeScene.cpp
@@ -36,9 +36,13 @@ bool CutTheRopeScene::init()
 		physics = PhysicsWorld::create();
 		if(NULL == physics) return false;
 
-		b2Body* brick1 = physics->createBrick(100,100,100,100,"brick1");
-		b2Body* brick2 = physics->createBrick(300,100,100,100,"brick2");
+		b2Body* brick1 = physics->createBrick(100,100,50,50,"brick1");
+		b2Body* brick2 = physics->createBrick(300,100,50,50,"brick2");
 		physics->createRope(brick1, brick2, 200 );
+
+		b2Body* bridge1 = physics->createStaticBrick(100,200,50,50,"bridge");
+		b2Body* bridge2 = physics->createStaticBrick(600,200,50,50,"bridge");
+		physics->createRope(bridge1,bridge2,550);
 
 		schedule(schedule_selector(CutTheRopeScene::updateWorld), physics->getUpdateTime());
 
